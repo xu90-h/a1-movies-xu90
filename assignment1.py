7c7c869d16c356d4b5e0a07a0eb1d6a8948c7833
huangxu <xu.huang@my.jcu.edu.au>
2025-06-16 13:37:17 +0800

    Add filename

--- assignment1.py
+++ assignment1.py
@@ -4,10 +4,12 @@ Date started: 06/14/2025
 GitHub URL: https://github.com/cp1404-students/a1-movies-xu90-h.git
 """
 
+FILENAME = "movies.csv"
 
 def main():
     """..."""
-    print("Must-See Movies 1.0 - by YourName")
+    print("Must-See Movies 1.0 - by Huang Xu")
+
 
 
 if __name__ == '__main__':
