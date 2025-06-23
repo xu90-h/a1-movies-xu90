3902bd09bdf559205a4307e755a787a19460c829
huangxu <xu.huang@my.jcu.edu.au>
2025-06-23 17:16:03 +0800

    Add main function with menu system

--- assignment1.py
+++ assignment1.py
@@ -11,8 +11,33 @@ STATUS_UNWATCHED = 'u'
 STATUS_WATCHED = 'w'
 
 def main():
-    """..."""
+    """Main function handling the menu loop."""
     print("Must-See Movies 1.0 - by Huang Xu")
+    movies = load_movies(FILENAME)
+    print(f"{len(movies)} movies loaded from {FILENAME}")
+
+    menu_choice = ""
+    while menu_choice != "Q":
+        print("Menu:")
+        print("D - Display movies")
+        print("A - Add new movie")
+        print("W - Watch a movie")
+        print("Q - Quit")
+        menu_choice = input(">>> ").strip().upper()
+
+        # Conduct menu selection
+        if menu_choice == "D":
+            display_movies(movies)
+        elif menu_choice == "A":
+            add_movie(movies)
+        elif menu_choice == "W":
+            watch_movie(movies)
+        elif menu_choice == "Q":
+            save_movies(FILENAME, movies)
+            print(f"{len(movies)} movies saved to {FILENAME}")
+            print("Have a nice day :)")
+        else:
+            print("Invalid menu choice")
 
 
 
