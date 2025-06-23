d5a166b1dd126ca0320a519fcfb3e72c349786db
huangxu <xu.huang@my.jcu.edu.au>
2025-06-23 14:22:17 +0800

    Set project structure and constants

--- assignment1.py
+++ assignment1.py
@@ -4,7 +4,11 @@ Date started: 06/14/2025
 GitHub URL: https://github.com/cp1404-students/a1-movies-xu90-h.git
 """
 
-FILENAME = "movies.csv"
+# Constants for file handling and data
+FILENAME = 'movies.csv'
+VALID_CATEGORIES = ["Action", "Comedy", "Documentary", "Drama", "Thriller", "Other"]
+STATUS_UNWATCHED = 'u'
+STATUS_WATCHED = 'w'
 
 def main():
     """..."""
